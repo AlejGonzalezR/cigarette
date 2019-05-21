b801c0fca40438568b4f7e7aeec6e391616c0db8
AlejGonzalezR <alexglzr994@gmail.com>
2019-05-21 18:55:19 -0500

    Avance con el navbar movil ggg

--- src/client/Nav.js
+++ src/client/Nav.js
@@ -1,10 +1,43 @@
 import React, { Component } from 'react';
 
+
 export default class Nav extends Component {
+    constructor(props) {
+        super(props)
+
+    }
+
+
     render() {
         return (
-            <div className="container">
-                hola
+            <div>
+                <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">Navbar</a>
+
+                    <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+                        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                            <li class="nav-item active">
+                                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Link</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+                <div class="position-relative overflow-hidden p-3 p-md-5  text-center bg-light hola">
+                    <div class="col-md-5 p-lg-5 mx-auto my-0">
+                        <h1 class="display-4 font-weight-normal">Punny headline</h1>
+                        <p class="lead font-weight-normal">And an even wittier subheading to boot. Jumpstart your marketing efforts with this example based on Apple’s marketing pages.</p>
+                        <a class="btn btn-outline-secondary" href="#">Coming soon</a>
+                    </div>
+                </div>
             </div>
         );
     }
