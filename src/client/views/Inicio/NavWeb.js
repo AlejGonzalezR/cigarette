3988e716a9ede13df17f2e3e833a849900ca1ebd
AlejGonzalezR <alexglzr994@gmail.com>
2019-08-29 09:57:44 -0500

    0

--- src/client/views/Inicio/NavWeb.js
+++ src/client/views/Inicio/NavWeb.js
@@ -1,42 +1,35 @@
 import React, { Component } from 'react';
 
 export default class NavWeb extends Component {
-  
+
 
     render() {
         return (
             <div className="display-nav-web">
                 <nav class="navbar navbar-expand-lg navbar-light nav-color ">
-                    
-                    
-                    <div class="collapse navbar-collapse justify-content-center order-2" id="navbarTogglerDemo">
-                   <a class="navbar-brand" href="https://github.com"><img className="nav-img2" src="img/brand.png" alt=""/></a>
-                    </div>
-                </nav>
-                <nav class="navbar navbar-expand-lg navbar-light nav-color2">
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
 
-                    <div class="collapse navbar-collapse justify-content-center order-2" id="navbarTogglerDemo03">
-                    <div class="d-flex">
-                        <ul class="navbar-nav mr-auto mt-2 mt-lg-0  ">
-                            <li class="nav-item nav-item-web ">
-                                <a class="nav-link2" href="https://github.com">Home <span class="sr-only">(current)</span></a>
-                            </li>
-                            <div class="dropdown-divider"></div>
-                            <li class="nav-item nav-item-web">
-                                <a class="nav-link2" href="https://github.com">Link</a>
-                            </li>
-                            <div class="dropdown-divider"></div>
-                            <li class="nav-item nav-item-web">
-                                <a class="nav-link2 " href="https://github.com" tabindex="-1" aria-disabled="true">Disabled</a>
-                            </li>
-                        </ul>
+
+                    <div class="collapse navbar-collapse nav-style order-2" id="navbarTogglerDemo">
+                        <a class="navbar-brand" href="https://github.com"><img className="nav-img2" src="img/brand.png" alt="" /></a>
+                        <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+                            <ul className="navbar-nav">
+                                <li className="nav-item">
+                                    <a className="nav-link text-uppercase" href="/inicio">Dashboard
+                                <span className="sr-only">(current)</span>
+                                    </a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link text-uppercase" href="/fqrequest">Group</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link text-uppercase" href="/reservations">reservations</a>
+                                </li>
+                               
+                            </ul>
                         </div>
                     </div>
                 </nav>
-                
+
             </div>
         );
     }
