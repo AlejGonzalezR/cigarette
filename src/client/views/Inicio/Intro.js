f4dc2865716400365fbc93bbe103c4a5aaf9375a
AlejGonzalezR <alexglzr994@gmail.com>
2019-10-28 11:55:15 -0500

    fff

--- src/client/views/Inicio/Intro.js
+++ src/client/views/Inicio/Intro.js
@@ -6,13 +6,11 @@ export default class Intro extends Component {
             <div className="row ">
                 <div className=" valign-wrapper ">
                 <div className="col s12 m12 l6 lx6  center-align">
-                <img src="https://images.unsplash.com/photo-1508138221679-760a23a2285b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80" className="responsive-img materialboxed" alt=""/>
+                <img src="https://images.unsplash.com/photo-1508138221679-760a23a2285b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80" className="imgsize-intro materialboxed" alt=""/>
                 </div>
                 <div className="col s12 m12 l6 lx6   justify-align">
-                <p><span className="intro-titulo span-titulo"></span> <span>El pasaje estándar Lorem Ipsum, usado desde el año 1500.</span></p>
-                
-                <p className="intro-subtitulo">  El pasaje estándar Lorem Ipsum, usado desde el año 1500.</p>
-                
+                <p><span className="span-titulo"></span> <span>El pasaje estándar Lorem Ipsum, usado desde el año 1500.</span></p>
+
                 <p className="fa">
                 El pasaje estándar Lorem Ipsum, usado desde el año 1500.
 "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
